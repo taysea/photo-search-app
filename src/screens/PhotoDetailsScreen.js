68964b1a601819ed80ab739268c74e422cb27b71
Taylor Seamans <taylor.seamans@hpe.com>
2019-08-07 16:22:48 -0700

    Create color palette linked to image

--- src/screens/PhotoDetailsScreen.js
+++ src/screens/PhotoDetailsScreen.js
@@ -3,14 +3,15 @@ import { Box, Text } from 'grommet';
 import {
   Guideline,
   ImageList,
-  PhotoDetails,
 } from '../components';
+
+import { Palette } from '../components/Palette';
 
 const PhotoDetailsScreen = props => (
   <Box gap="medium">
     <Guideline tip="This page fetches data specific to the image that was clicked— including information about the user who uploaded the image and statistics regarding number of views, likes, and downloads." width="large" />
 
-    <PhotoDetails {...props} />
+    <Palette {...props} />
 
     <Text as="h2" size="xlarge" textAlign="center">other photos you might like</Text>
 
