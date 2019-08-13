b12cdb881922e82d22f161fb6579022facaf34d2
karatehcops <contact@lazers.tv>
2019-08-13 10:25:48 -0400

    render props example.

--- src/screens/PhotoDetailsScreen.js
+++ src/screens/PhotoDetailsScreen.js
@@ -10,6 +10,22 @@ import {
 import LoadingScreen from './LoadingScreen';
 // import { Palette } from '../components/Palette';
 
+// see https://medium.com/front-end-weekly/data-fetcher-component-using-hooks-and-render-props-aacf3162dfc2
+/*
+
+const PhotoData = ({ photoId }) => {
+  let isLoading = true; 
+ const res = async () => {
+   await fetch('')
+  };
+  // fetching logic
+
+  return { myPhotoData, isLoading };
+
+}
+
+*/
+
 const PhotoDetailsScreen = (props) => {
   const [isPalleteLoading, setPalleteLoading] = useState(true);
   const [isListLoading, setListLoading] = useState(true);
@@ -20,7 +36,11 @@ const PhotoDetailsScreen = (props) => {
     >
       {(isPalleteLoading || isListLoading) && <LoadingScreen />}
       <Palette setPageLoadingStatus={status => setPalleteLoading(status)} {...props} />
-
+      {/* Render props pattern
+      <PhotoData photoId={123445}>
+        {({myPhotoData, isLoading}) => isLoading ? <MyLoader> : <Pallete photo={myPhotoData}>}
+      </PhotoData>
+      */}
       <Box>
         <Text size="large" textAlign="center" margin="none">still looking for something different?</Text>
       </Box>
